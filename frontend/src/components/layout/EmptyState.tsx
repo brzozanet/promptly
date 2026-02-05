195290ee5069fee33974f503156f7b11691f2e9c
Paweł Brzozowski <pawel@brzoza.net>
2026-02-05 13:45:33 +0100

    Update EmptyState component text for improved clarity and styling

--- frontend/src/components/layout/EmptyState.tsx
+++ frontend/src/components/layout/EmptyState.tsx
@@ -5,7 +5,11 @@ export function EmptyState() {
   return (
     <div className="mx-auto flex min-h-[calc(100vh-200px)] w-full max-w-5xl flex-col justify-center">
       <h1 className="text-3xl ml-4 text-white text-left mb-6 font-light">
-        Porozmawiaj z Promptly o fotografii...
+        Porozmawiaj z{" "}
+        <span className="bg-linear-to-r from-indigo-500 from-5% via-sky-500 via-15% to-emerald-500 to-80% bg-clip-text text-transparent font-semibold">
+          Promptly
+        </span>{" "}
+        o fotografii
       </h1>
       <h2 className="text-5xl ml-4 bg-linear-to-r from-indigo-500 from-5% via-sky-500 via-15% to-emerald-500 to-80% bg-clip-text text-transparent text-left mb-6 font-medium">
         <span className="text-white">Jak </span>
